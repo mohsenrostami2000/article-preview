9cc5b0738856f670cf75acb28e3b9c3b53d3fc1d
mohsen <mohsenrostami248@gmail.com>
2023-12-29 01:19:51 +0330

    finish js

---
+++ script.js
@@ -0,0 +1,24 @@
+// select items ------------------------
+const shareBtn = document.getElementById('share');
+const shareBtnTwo = document.querySelector('.share2')
+const profile = document.querySelector('.profile');
+const phoneLinks = document.querySelector('.social-medias');
+const desktopLinks = document.querySelector('.desktop-links');
+
+// Events ----------------------------------------------    
+
+shareBtn.addEventListener('click', () => {
+    if(window.innerWidth < 1000) {
+        phoneLinks.classList.toggle('active');
+        profile.classList.toggle('in-active');
+    } else {
+        desktopLinks.classList.toggle('active');
+        
+    }
+});
+
+shareBtnTwo.addEventListener('click', () => {
+    phoneLinks.classList.toggle('active');
+    profile.classList.toggle('in-active');
+});
+
